8dd68fee7674afef0dba3ce4b0680bab6e2dfadd
azimjonovamadina967-ship-it <azimjonovamadina967@gmail.com>
2025-11-17 10:04:33 +0500

    Create hw-1.py

---
+++ lesson-1/homework/hw-1.py
@@ -0,0 +1,34 @@
+## lesson-1
+a = float(input("Kvadratning tomonini kiriting: "))
+
+p = 4 * a
+ar = a ** 2
+
+print("Perimetri:", p)
+print("Yuzasi:", ar)
+
+d = float(input("Aylananing diametrini kiriting: "))
+
+C = 3.14 * d
+
+print("Aylananing uzunligi:", C)
+
+a = float(input("a sonini kiriting: "))
+b = float(input("b sonini kiriting: "))
+
+mean = (a + b) / 2
+
+print("O'rtacha qiymat:", mean)
+
+a = float(input("a ni kiriting: "))
+b = float(input("b ni kiriting: "))
+
+summa = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("total:", summa)
+print("product:", product)
+print("a  square:", square_a)
+print("b square:", square_b)
