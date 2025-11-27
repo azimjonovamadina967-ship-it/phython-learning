fe0884d9e7bbe6aa08af1e03dd133cb28811ecf5
azimjonovamadina967-ship-it <azimjonovamadina967@gmail.com>
2025-11-27 08:39:11 +0500

    Update hw-5.py

--- lesson-5/homework/hw-5.py
+++ lesson-5/homework/hw-5.py
@@ -1,11 +1,23 @@
 def is_leap(year):
+    # Input validation must be at the top
+    if not isinstance(year, int):
+        raise ValueError("Year must be an integer.")
+
+    # Leap year rules
     if year % 400 == 0:
         return True
     if year % 100 == 0:
         return False
     if year % 4 == 0:
         return True
-print(is_leap(20000))
+
+    return False  # default case
+
+
+# Test cases
+print(is_leap(2000))   # True
+print(is_leap(1900))   # False
+print(is_leap(20000))  # False
 
 n = int(input())
 
@@ -18,12 +30,21 @@ elif 6 <= n <= 20:
 else:
     print("Not Weird")
 
-a = int(input())
-b = int(input())
+a = int(input("a = "))
+b = int(input("b = "))
 
-# Agar a toq bo'lsa, uni birga oshirib juft qilamiz
+# if-else bilan birinchi juft sonni topamiz
 if a % 2 != 0:
     a += 1
 
 evens = list(range(a, b + 1, 2))
-print(evens)
+
+print("Even numbers:", *evens)
+a = int(input("a = "))
+b = int(input("b = "))
+
+start = a + (a % 2)   # NO if-else
+
+evens = list(range(start, b + 1, 2))
+
+print("Even numbers:", *evens)
