3fcec98d113262e932358842559d175a09d2cafd
azimjonovamadina967-ship-it <azimjonovamadina967@gmail.com>
2025-12-04 00:51:20 +0500

    Update hw-6.py

--- lesson-6/homework/hw-6.py
+++ lesson-6/homework/hw-6.py
@@ -48,13 +48,100 @@ while i <= rows:
     i += 1
 
 ### Exercise 3: Calculate sum of all numbers from 1 to a given number
-number = 10
+number =10
 sum = 0
 for i in range(1, number + 1):
     sum += i
 print(sum)
 
+### Exercise 4: Print multiplication table of a given number
+num = 2
+for i in range(1, 11):
+    print(num * i)
 
 
+### Exercise 5: Display numbers from a list using a loop
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for x in numbers:
+    if x > 500:
+        break
+    if x == 75:         
+        print(x)
+    elif x > 100 and x != 180:   # 100 dan katta va 180 ga teng bo‘lmasa
+        print(x)
 
 
+### Exercise 6: Count the total number of digits in a number
+num = int(input('Enter numbers'))
+print (len(str(num)))
+### Exercise 7: Print reverse number pattern
+rows = 5
+for i in range(0, rows + 1):
+    for j in range(rows - i, 0, -1):
+        print(j, end=' ')
+    print()
+### Exercise 8: Print list in reverse order using a loop
+list1 = [10, 20, 30, 40, 50]
+for i in reversed (list1):
+ print(i)
+
+
+### Exercise 9: Display numbers from -10 to -1 using a for loop
+for i in range(-10,-0,1):
+ print(i)
+### Exercise 10: Display message “Done” after successful loop execution
+for i in range(5):
+    print(i)
+else:
+    print("Done")
+
+### Exercise 11: Print all prime numbers within a range
+
+for num in range(25, 51):  # 25 dan 50 gacha
+    if num > 1:             # 1 dan katta sonlarni tekshiramiz
+        for i in range(2, num):
+            if num % i == 0:
+                break       # bo‘linadigan son topildi → prime emas
+        else:
+            print(num)      # for loopni to‘liq tugatgan bo‘lsa → prime
+
+### Exercise 12: Display Fibonacci series up to 10 terms
+n = 10
+a = 0
+b = 1
+next = b  
+count = 1
+
+while count <= n:
+    print(next, end=" ")
+    count += 1
+    a, b = b, next
+    next = a + b
+print()
+### Exercise 13: Find the factorial of a given number
+# Python program to find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = 5
+
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
+## 14. Return Uncommon Elements of Lists
+list1 = [1, 1, 2]
+list2 = [2, 3, 4]
+uncommon = [x for x in list1 if x not in list2] + [x for x in list2 if x not in list1]
+print(uncommon)
